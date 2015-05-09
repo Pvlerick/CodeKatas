07076926561573073bf35ce04c88681044f2f25d
Philippe <pvlerick@gmail.com>
2015-05-09 18:57:21 +0200

    StringNumbers kata ammeneded, step 5

--- FSharp/CodeKatas/StringCalculator/StringCalculator1.fs
+++ FSharp/CodeKatas/StringCalculator/StringCalculator1.fs
@@ -7,14 +7,18 @@ let add numbers =
     match numbers with
     | "" -> 0
     | _ ->
-        let parse (content: string) (delim: char) =
-            content.Split('\n')
-            |> Seq.collect (fun l -> l.Split(delim))
-            |> Seq.sumBy Int32.Parse
-        if numbers.StartsWith("//") then
-            parse (numbers.Substring 4) numbers.[2]
-        else parse numbers ','
+        let parse (content: string) delimiters =
+            content.Split(delimiters)
+            |> Seq.map Int32.Parse
 
+        let ns =
+            if numbers.StartsWith("//")
+                then parse (numbers.Substring 4) [| '\n'; numbers.[2] |]
+                else parse numbers [| '\n'; ',' |]
+
+        if ns |> Seq.exists (fun i -> i < 0)
+            then failwith ("negatives not allowed: " + String.Join(",", (ns |> Seq.filter (fun i -> i < 0) |> Seq.toArray)))
+            else ns |> Seq.sum
 
 [<Fact>]
 let Add_With_EmptyString_Must_ReturnZero() =
@@ -35,7 +39,7 @@ let Add_With_OneNumber_Must_ReturnNumberAsInt(value: string, expected: int) =
 [<InlineData("1,5", 6)>]
 [<InlineData("42,0", 42)>]
 [<InlineData("0,42", 42)>]
-[<InlineData("42,-5", 37)>]
+[<InlineData("42,5", 47)>]
 let Add_With_TwoNumbers_Must_ReturnSumOfNumbersAsInt(value: string, expected: int) =
     let result = add value
     Assert.Equal(expected, result)
@@ -44,7 +48,7 @@ let Add_With_TwoNumbers_Must_ReturnSumOfNumbersAsInt(value: string, expected: in
 [<InlineData("0,1,2", 3)>]
 [<InlineData("1,5,8,9", 23)>]
 [<InlineData("0,1,2,3,5,8,13,21", 53)>]
-[<InlineData("0,10,0,5,-8", 7)>]
+[<InlineData("0,10,0,5,8", 23)>]
 let Add_With_UnknownAmountOfNumbers_Must_ReturnSumOfNumbersAsInt(value: string, expected: int) =
     let result = add value
     Assert.Equal(expected, result)
@@ -62,3 +66,11 @@ let Add_With_UnknownAmountOfNumbersWithLineBreaks_Must_ReturnSumOfNumbersAsInt(v
 let Add_With_UnknownAmountOfNumbersWithDifferentDelimiter_Must_ReturnSumOfNumbersAsInt(value: string, expected: int) =
     let result = add value
     Assert.Equal(expected, result)
+
+[<Theory>]
+[<InlineData("-3,5", "-3")>]
+[<InlineData("-2,3,-9", "-2,-9")>]
+[<InlineData("//*\n-7*3\n-2", "-7,-2")>]
+let Add_With_UnknownAmountOfNumbersWithNegatives_Must_ThrowWithNegativesInTheMessage(value: string, expected: string) =
+    let e = Assert.Throws<Exception>(fun () -> add value |> ignore)
+    Assert.Equal("negatives not allowed: " + expected, e.Message)
